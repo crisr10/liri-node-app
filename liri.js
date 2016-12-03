93fef8f7d1eec4fba43becb92cc9cb1604260f84
Cristian Rodriguez <cristian_rocas@hotmail.com>
2016-12-02 17:50:14 -0800

    Got the spotify API and the Twitter API to respond, now trying to manipulate the data and pull only what needed

--- liri.js
+++ liri.js
@@ -1,17 +1,10 @@
 var action = process.argv[2];
-console.log(process.argv[3]);
 var keys = require('./keys.js');
-
-if (action==='movie-this') {
-
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-var request = require("request");
+var title = "";
+function multipleWords () {
 
 // Store all of the arguments in an array
 var nodeArgs = process.argv;
-
-// Create an empty variable for holding the movie name
-var movieName = "";
 
 // Loop through all the words in the node argument
 // And do a little for-loop magic to handle the inclusion of "+"s
@@ -20,19 +13,26 @@ var movieName = "";
 
 		  if (i > 2 && i < nodeArgs.length) {
 
-		    movieName = movieName + "+" + nodeArgs[i];
+		    title = title + "+" + nodeArgs[i];
 
 		  }
 
 		  else {
 
-		    movieName += nodeArgs[i];
+		    title += nodeArgs[i];
 
 		  }
 		}
+}
+
+if (action==='movie-this') {
+
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+multipleWords();
 
 		// Then run a request to the OMDB API with the movie specified
-		var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+		var queryUrl = "http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json";
 
 		// This line is just to help us debug against the actual URL.
 		console.log(queryUrl);
@@ -57,11 +57,34 @@ var movieName = "";
 	});
 } 
 else if (action==='my-tweets') {
+	var Twitter = require('twitter');
+	var twitterKeys = keys.twitterKeys;
+
+	var client = new Twitter({twitterKeys});
+
+	console.log('this is the client'+ client);
 	var params = {screen_name: 'nodejs'};
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		  	if (!error) {
-		    console.log(JSON.stringify(tweets, null, 2));
-		 	}
-		});
-} 
-else if (action==='spotify-this-song')
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    console.log(tweets.text);
+	  }
+	});
+}
+else if (action==='spotify-this-song'){
+	var spotify = require('spotify');
+ 	
+ 	multipleWords();
+ 	console.log(title);
+
+spotify.search({ type: 'track', query: title, limit:1 }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    console.log(data);
+    console.log('Artist: '+data.tracks.items[0].album.artists[0].name);
+    console.log('Song: '+data.tracks.items[0].name);
+    console.log('Preview Link: '+data.tracks.items[0].preview_url);
+    console.log('Album: '+data.tracks.items[0].album.artists[0].name);
+});
+}
